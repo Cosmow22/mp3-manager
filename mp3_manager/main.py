279b73a2e9427dd17904f7f5519ad0490c3fb2d5
Cosmow22 <cosmow543@gmail.com>
2025-01-04 12:07:08 +0100

    add subparser & path argument not required

--- mp3_manager/main.py
+++ mp3_manager/main.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-from mp3_manager import scan, edit
+from mp3_manager import scan, edit, equalize
 
 
 def cli():
@@ -8,15 +8,16 @@ def cli():
         description="A CLI to manage mp3."
         )
     parser.add_argument(
-        "path",
-        help="The path to mp3."
+        "-p", "--path",
+        help="The path to mp3.",
+        default=".",
         )
     subparsers = parser.add_subparsers()
     
     scan_parser = subparsers.add_parser("scan", help="Scan the folder and create a csv file.")
     scan_parser.set_defaults(func=scan)
     
-    edit_parser = subparsers.add_parser("edit", help="Parse csv file and edit songs metadata.")
+    edit_parser = subparsers.add_parser("edit", help="Parse csv file and edit musics metadata.")
     edit_parser.add_argument(
         "-csv",
         "--csv", 
@@ -24,8 +25,10 @@ def cli():
         )
     edit_parser.set_defaults(func=edit)
     
+    equalize_parser = subparsers.add_parser("equalize", help="Equalize the volume of all musics.")
+    equalize_parser.set_defaults(func=equalize)
+    
     args = parser.parse_args()
-    print(args)
     args.func(args)
 
 
