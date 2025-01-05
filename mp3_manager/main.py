faf756e5887379fc6aef36d5c2e10e8774227971
Cosmow22 <cosmow543@gmail.com>
2025-01-05 08:45:36 +0100

    arguments: change csv, add dBFS

--- mp3_manager/main.py
+++ mp3_manager/main.py
@@ -12,17 +12,23 @@ def cli():
         help="The path to mp3.",
         default=".",
         )
+    parser.add_argument(
+        "-csv",
+        "--csv", 
+        help="The path to csv file.",
+        default="musics.csv",
+        )
+    parser.add_argument(
+        "-dBFS",
+        help="The path to csv file.",
+        default=-14,
+        )
     subparsers = parser.add_subparsers()
     
     scan_parser = subparsers.add_parser("scan", help="Scan the folder and create a csv file.")
     scan_parser.set_defaults(func=scan)
     
     edit_parser = subparsers.add_parser("edit", help="Parse csv file and edit musics metadata.")
-    edit_parser.add_argument(
-        "-csv",
-        "--csv", 
-        help="The path to csv file.",
-        )
     edit_parser.set_defaults(func=edit)
     
     equalize_parser = subparsers.add_parser("equalize", help="Equalize the volume of all musics.")
