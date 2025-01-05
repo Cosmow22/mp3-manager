f66fc4458db853f144a7012067382be1af4c7ee2
Cosmow22 <cosmow543@gmail.com>
2025-01-05 14:45:37 +0100

    add terminal.py, HiddenPrints

---
+++ mp3_manager/terminal.py
@@ -0,0 +1,19 @@
+import os, sys
+
+
+class HiddenPrints:
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+        
+        
+def print_to(line, music_name):
+    print(f"\033[{5-line}A", end="")  # move cursor up
+    print("\033[2K", end="")  # clear line
+    print(f"Thread {line}: {music_name}", end="\r")
+    print(f"\033[{5-line}B", end="")  # move cursor back
+    
