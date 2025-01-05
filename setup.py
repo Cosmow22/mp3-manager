902ef95488bb425f12501ede63c65c183c2a31d8
Cosmow22 <cosmow543@gmail.com>
2025-01-05 17:11:53 +0100

    2.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mp3_manager",
-    version="2.0.1",
+    version="2.0.2",
     packages=find_packages(),
     install_requires=[
         "eyed3>=0.9.7",
